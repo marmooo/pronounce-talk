8e047792c3f0e73f06477e839882ec3dedc97bc8
marmooo <marmooo@users.noreply.github.com>
2021-10-17 13:51:22 +0900

    bump bootstrap from 5.1.2 to 5.1.3

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = '2021-10-15 20:33';
+var CACHE_NAME = '2021-10-17 13:42';
 var urlsToCache = [
   "/pronounce-talk/",
   "/pronounce-talk/index.js",
@@ -13,7 +13,7 @@ var urlsToCache = [
   "/pronounce-talk/mp3/correct3.mp3",
   "/pronounce-talk/favicon/original.svg",
   "https://marmooo.github.io/fonts/textar-light.woff2",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/animate.css@4.1.1/animate.min.css",
 ];
 
